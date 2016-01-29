a42e1049014f8e284e90ab25db80d4ac49bdb263
KinsomyJS <FJS0420@outlook.com>
2016-01-29 20:47:11 +0800

    Revert "播放器模块"
    This reverts commit 2db18ce4cd603b6f5511f695d9f6473c0222fd37.

--- GreenPlayer/app/src/main/java/com/example/greenplayer/ui/localVideo.java
+++
@@ -1,7 +0,0 @@
-package com.example.greenplayer.ui;
-
-/**
- * Created by FJS0420 on 2016/1/27.
- */
-public class localVideo {
-}
--- GreenPlayer/oPlayer/src/main/java/io/vov/vitamio/R.java
+++
@@ -1,8 +0,0 @@
-package io.vov.vitamio;
-
-public class R {
-	public static final class raw {
-		public static final int libarm = com.nmbb.oplayer.R.raw.libarm;
-		public static final int pub = com.nmbb.oplayer.R.raw.pub;
-	}
-}
